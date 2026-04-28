7ec08f6f57dd558c548ef3691f434000881879cb
user.email <mail@akshaydeshraj.me>
2026-04-28 21:16:50 +0530

    chore(dotfiles): sync configs 2026-04-28

--- doom/.config/doom/packages.el
+++ doom/.config/doom/packages.el
@@ -52,3 +52,7 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
+
+(package! shell-maker)
+(package! acp)
+(package! agent-shell)
